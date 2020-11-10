07bdfd77a56a1aac79f221c46635c2d7a1dde23a
arvi17 <ing.armandovives17@gmail.com>
2020-11-10 09:13:14 -0500

    Se modifica admin.py principal

--- sped/principal/admin.py
+++ sped/principal/admin.py
@@ -3,10 +3,27 @@ from principal import models
 
 # Register your models here.
 
-class SolicitudAdmin(admin.ModelAdmin):
-    list_display=("id","nombre")
+class RegimenAdmin(admin.ModelAdmin):
+    list_display=("id","nombre","descripcion")  
 
+class TipoSolicitanteAdmin(admin.ModelAdmin):
+    list_display=("id","nombre","descripcion")
 
+class DisciplinaDeportivaAdmin(admin.ModelAdmin):
+    list_display=("id","nombre","descripcion")  
 
+class EscenarioDeportivoAdmin(admin.ModelAdmin):
+    list_display=("id","nombre","descripcion")  
 
-admin.site.register(models.Solicitud,SolicitudAdmin)
+class ActividadDeportivaAdmin(admin.ModelAdmin):
+    list_display=("id","nombre","descripcion")  
+
+class  SolicitudAdmin(admin.ModelAdmin):
+    list_display=("id","nombre","tiposolicitante","direccion","barrio","correo","tipoidentificacion","telefono","sexo","discapacidad","adjuntocedula","adjuntorut","escenario","tipoevento","actividaddeportiva","descripcion")      
+    
+admin.site.register(models.Solicitud, SolicitudAdmin)
+admin.site.register(models.Regimen, RegimenAdmin)
+admin.site.register(models.TipoSolicitante, TipoSolicitanteAdmin)
+admin.site.register(models.DisciplinaDeportiva, DisciplinaDeportivaAdmin)
+admin.site.register(models.EscenarioDeportivo, EscenarioDeportivoAdmin)
+admin.site.register(models.ActividadDeportiva, ActividadDeportivaAdmin)
--- sped/principal/models.py
+++ sped/principal/models.py
@@ -7,21 +7,29 @@ class TipoSolicitante(models.Model):
     id=models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=30)
     descripcion = models.CharField(max_length=30)
+    def __str__(self):
+        return (self.nombre) 
 
 class DisciplinaDeportiva(models.Model):
     id=models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=30)
     descripcion = models.CharField(max_length=30)
+    def __str__(self):
+        return (self.nombre) 
 
 class EscenarioDeportivo(models.Model):
     id=models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=30)
     descripcion = models.CharField(max_length=30)
+    def __str__(self):
+        return (self.nombre) 
 
 class ActividadDeportiva(models.Model):
     id=models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=30)
     descripcion = models.CharField(max_length=30)
+    def __str__(self):
+        return (self.nombre) 
 
 class TipoIdentificacion(Enum):
     cc = "CC"
@@ -36,11 +44,15 @@ class Discapacidad(models.Model):
     id=models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=30)
     descripcion = models.CharField(max_length=30)
+    def __str__(self):
+        return (self.nombre) 
 
 class Regimen(models.Model):
     id=models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=30)
     descripcion = models.CharField(max_length=30)
+    def __str__(self):
+        return (self.nombre) 
 
 class Solicitud(models.Model):
     id=models.AutoField(primary_key=True)
@@ -60,3 +72,5 @@ class Solicitud(models.Model):
     actividaddeportiva =models.ForeignKey(ActividadDeportiva, default="", on_delete=models.CASCADE)
     descripcion =  models.CharField(max_length=30)
     eventodeportivo = models.ForeignKey(Regimen, default="", on_delete=models.CASCADE)
+    def __str__(self):
+        return (self.nombre) 
