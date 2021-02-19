f0bafe2d6ac1eab8aad53a84648ce39ce3de2785
Cristhian Carpio <cristhian.carpio18@gmail.com>
2021-02-18 23:17:02 -0500

    Se cambia conexión a base de datos MYSQL

--- sped/sped/settings.py
+++ sped/sped/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
 
-        --------------Desarrollo---------------#
+        #--------------Desarrollo---------------
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'sped',       
         'USER': 'inreddos_siru',  
